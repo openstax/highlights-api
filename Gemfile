faa6f5ddbe6e0aec9ad190a74963898acaf8f948
Steve Burkett <steve@burkettnet.com>
2019-10-22 09:43:22 -0700

    Add specs & factory-bot

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ group :test do
 end
 
 group :development, :test do
+  gem 'factory_bot_rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 
--- spec/models/highlight_spec.rb
+++ spec/models/highlight_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Highlight, type: :model do
+  let(:highlight) { build(:highlight) }
+
+  describe 'creates to db ok' do
+    before { create(:highlight) }
+
+    it 'has a count of 1 in the db' do
+      expect(Highlight.count).to eq 1
+    end
+  end
+
   describe 'validations' do
     describe '#source_type' do
       it 'must be a valid enum' do
@@ -22,6 +32,15 @@ RSpec.describe Highlight, type: :model do
         record = described_class.new(color: '#C0C0C0')
         expect(record.valid?).to be_truthy
         expect(record.errors[:color]).to_not include('is invalid')
+      end
+    end
+
+    describe '#user_uuid' do
+      let(:invalid_user_uuid) { 'foobar' }
+      let(:foobar_highlight) { build(:highlight, user_uuid: invalid_user_uuid) }
+
+      it 'must be a uuid' do
+        expect(foobar_highlight.user_uuid).to be_nil
       end
     end
   end
