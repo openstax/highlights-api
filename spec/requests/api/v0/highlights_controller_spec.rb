af685bd8b842bab8dedcb1dd8b9ab60a4cf7c8dd
Steve Burkett <steve@burkettnet.com>
2019-10-24 11:50:28 -0700

    Add spec for invalid request

--- spec/requests/api/v0/highlights_controller_spec.rb
+++ spec/requests/api/v0/highlights_controller_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe HighlightsController, type: :request do
+RSpec.describe Api::V0::HighlightsController, type: :request do
   # let!(:highlights) { create_list(:highlight, 10) }
 
   describe 'POST /highlights' do
     let(:valid_attributes) { { title: 'Learn Elm', created_by: '1' } }
 
     context 'when the request is valid' do
-      before { post '/highlights', params: valid_attributes }
+      before { post '/api/v0/highlights', params: valid_attributes }
 
       it 'creates a highlight' do
         expect(json_response['user_uuid']).to eq('user_uuid')
@@ -18,17 +18,17 @@ RSpec.describe HighlightsController, type: :request do
       end
     end
 
-    # context 'when the request is invalid' do
-    #   before { post '/todos', params: { title: 'Foobar' } }
-    #
-    #   it 'returns status code 422' do
-    #     expect(response).to have_http_status(422)
-    #   end
-    #
-    #   it 'returns a validation failure message' do
-    #     expect(response.body)
-    #       .to match(/Validation failed: Created by can't be blank/)
-    #   end
-    # end
+    context 'when the request params are invalid' do
+      before { post '/api/v0/highlights', params: { something: 'Foobar' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/param is missing or the value is empty/)
+      end
+    end
   end
 end
