2abdff6dec714d50ebc9b16d0165b4d65b149603
Steve Burkett <steve@burkettnet.com>
2019-10-24 11:50:28 -0700

    Add spec for invalid request

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -6,7 +6,7 @@ module ExceptionHandler
       json_response({ message: e.message }, :not_found)
     end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
+    rescue_from ActiveRecord::RecordInvalid, ActionController::ParameterMissing do |e|
       json_response({ message: e.message }, :unprocessable_entity)
     end
   end
--- app/serializers/highlight_serializer.rb
+++ app/serializers/highlight_serializer.rb
@@ -1,3 +1,3 @@
 class HighlightSerializer < ActiveModel::Serializer
-  attributes :id, :source_type, :source_id, :location_strategies
+  attributes :id, :source_type, :user_uuid, :source_id, :location_strategies
 end
