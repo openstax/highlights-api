fc26ea8b817b5193a39a777e99870239cccefec3
Steve Burkett <steve@burkettnet.com>
2019-11-04 12:56:56 -0800

    Add a binding from model creation method

--- spec/requests/api/v0/highlights_controller_spec.rb
+++ spec/requests/api/v0/highlights_controller_spec.rb
@@ -2,13 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Api::V0::HighlightsController, type: :request do
   describe 'POST /highlights' do
-    let(:valid_attributes) { { title: 'Learn Elm', created_by: '1' } }
+    let(:valid_attributes) do
+      {
+        highlight:
+        {
+          source_id: 'foo id',
+          anchor: 'foo anchor',
+          highlighted_content: 'foo content',
+          source_type: 'openstax_page',
+          color: '#000000',
+          location_strategies: [type: 'TextPositionSelector',
+                                start: 12,
+                                end: 10]
+        }
+      }
+    end
 
     context 'when the request is valid' do
       before { post '/api/v0/highlights', params: valid_attributes }
 
       it 'creates a highlight' do
-        expect(json_response['user_uuid']).to eq('user_uuid')
+        expect(json_response[:anchor]).to eq('foo anchor')
       end
 
       it 'returns status code 201' do
@@ -17,15 +31,34 @@ RSpec.describe Api::V0::HighlightsController, type: :request do
     end
 
     context 'when the request params are invalid' do
-      before { post '/api/v0/highlights', params: { something: 'Foobar' } }
+      context 'bogus params' do
+        before { post '/api/v0/highlights', params: { something: 'Foobar' } }
 
-      it 'returns status code 422' do
-        expect(response).to have_http_status(422)
+        it 'returns status code 422' do
+          expect(response).to have_http_status(422)
+        end
+
+        it 'returns a validation failure message' do
+          expect(response.body)
+            .to match(/param is missing or the value is empty/)
+        end
       end
 
-      it 'returns a validation failure message' do
-        expect(response.body)
-          .to match(/param is missing or the value is empty/)
+      context 'when the location_strategies are invalid' do
+        before do
+          new_attributes = valid_attributes.clone
+          new_attributes[:highlight][:location_strategies] = [type: 'Foobar']
+          post '/api/v0/highlights', params: new_attributes
+        end
+
+        it 'returns status code 422' do
+          expect(response).to have_http_status(422)
+        end
+
+        it 'returns a validation failure message' do
+          expect(response.body)
+            .to match(/invalid strategy detected/)
+        end
       end
     end
   end
