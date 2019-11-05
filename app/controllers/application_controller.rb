37edde032bfbc8ff391fb388f1cd092730c35e11
JP Slavinsky <jpslav@gmail.com>
2019-11-04 17:31:10 -0700

    get user uuid from sso cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::API
 
   include RescueFromUnlessLocal
 
+  def current_user_uuid
+    if Rails.environment.development? && ENV['FORCED_USER_UUID']
+      ENV['FORCED_USER_UUID']
+    else
+      OpenStax::Accounts::Sso.user_uuid(request)
+    end
+  end
+
 end
