2b4d3162d58ba2ef4aa216a45a30d661d451a71e
JP Slavinsky <jpslav@gmail.com>
2019-11-06 06:15:49 -0700

    add 'me' diagnostic endpoint for testing accounts integration

--- app/controllers/api/v0/diagnostics_controller.rb
+++ app/controllers/api/v0/diagnostics_controller.rb
@@ -1,11 +1,18 @@
 class Api::V0::DiagnosticsController < Api::V0::BaseController
 
+  # Forcing an exception
   def exception
     raise "An exception for diagnostic purposes"
   end
 
+  # Forcing a status code response
   def status_code
     head params[:status_code]
+  end
+
+  # Showing whether Accounts integration is working
+  def me
+    render json: { uuid: current_user_uuid }
   end
 
 end
