b4467d56a36b854029dda78299933bb44d01b3f0
JP Slavinsky <jpslav@gmail.com>
2019-11-10 17:12:54 -0700

    report 500s to sentry when rescue them in prod

--- app/controllers/api/v0/base_controller.rb
+++ app/controllers/api/v0/base_controller.rb
@@ -2,7 +2,7 @@ class Api::V0::BaseController < ApplicationController
   include Swagger::Blocks
   include OpenStax::Swagger::Bind
 
-  rescue_from_unless_local StandardError do |ex|
+  rescue_from_unless_local StandardError, send_to_sentry: true do |ex|
     render json: binding_error(status_code: 500, messages: [ex.message]), status: 500
   end
 
