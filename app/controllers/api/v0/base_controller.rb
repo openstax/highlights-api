dbd7864f4390a1d382992efcba68a26e5d816b58
JP Slavinsky <jpslav@gmail.com>
2019-12-06 16:23:46 -0700

    more service limit exception handler to make it catch

--- app/controllers/api/v0/base_controller.rb
+++ app/controllers/api/v0/base_controller.rb
@@ -2,10 +2,6 @@ class Api::V0::BaseController < ApplicationController
   include Swagger::Blocks
   include OpenStax::Swagger::Bind
 
-  rescue_from_unless_local ServiceLimits::ServiceLimitsError, send_to_sentry: true do |ex|
-    render json: binding_error(status_code: 403, messages: [ex.message]), status: 403
-  end
-
   rescue_from_unless_local StandardError, send_to_sentry: true do |ex|
     render json: binding_error(status_code: 500, messages: [ex.message]), status: 500
   end
@@ -20,6 +16,10 @@ class Api::V0::BaseController < ApplicationController
 
   rescue_from_unless_local SecurityTransgression do |ex|
     head :forbidden
+  end
+
+  rescue_from_unless_local ServiceLimits::ServiceLimitsError, send_to_sentry: true do |ex|
+    render json: binding_error(status_code: 403, messages: [ex.message]), status: 403
   end
 
   protected
